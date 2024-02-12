a56e504b091c51ce741f8455aa636330c059eb17
elisa <elisacss95@gmail.com>
2024-02-12 10:47:00 -0300

    first commit

---
+++ Heroi.js
@@ -0,0 +1,32 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+        switch (this.tipo) {
+            case 'mago':
+                ataque = 'magia';
+                break;
+            case 'guerreiro':
+                ataque = 'espada';
+                break;
+            case 'monge':
+                ataque = 'artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'shuriken';
+                break;
+            default:
+                ataque = 'ataque';
+        }
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+const heroi1 = new Heroi('Naruto', 17, 'ninja');
+
+heroi1.atacar();
